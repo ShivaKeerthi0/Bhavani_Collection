3c8fe845cfcd627c49fecdbdcb329aaa97ae0078
ShivaKeerthi0 <60295265+ShivaKeerthi0@users.noreply.github.com>
2020-07-19 14:51:53 +0530

    Update Collection.java

--- Collections/src/main/java/com/maven/Collections/Collection.java
+++ Collections/src/main/java/com/maven/Collections/Collection.java
@@ -1,39 +1,23 @@
 package com.maven.Collections;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Collection
 {
-	private static Logger LOGGER=LogManager.getLogger(Collection.class); 
+	private static Logger LOG=LogManager.getLogger(Collection.class); 
     public static void main(String[] args) 
     {
-        DataList<Integer> list = new DataList<Integer>();
- 
-  
+        DataList<Integer> list = new DataList();
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(10);
-        LOGGER.info(list);
-      
-        list.remove(2);
-        LOGGER.info(list);
-        
-        LOGGER.info( list.get(0) );
-        LOGGER.info( list.get(4) );
-        LOGGER.info( list.get(6) );
-        LOGGER.info( list.get(8) );
-       
-
- 
-  
-        LOGGER.info(list.size());
+        LOG.info(list);
+        LOG.info( list.get(0));
+        LOG.info( list.get(2));
+	 list.remove(2);
+	    
+        LOG.info(list.size());
     }
 }
